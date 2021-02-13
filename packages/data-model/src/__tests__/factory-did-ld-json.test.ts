2dca2d8702d2482fd0eff0403c390eb148a9b6cf
Orie Steele <orie@transmute.industries>
2021-02-13 11:37:55 -0600

    handle context

--- packages/data-model/src/__tests__/factory-did-ld-json.test.ts
+++ packages/data-model/src/__tests__/factory-did-ld-json.test.ts
@@ -1,6 +1,9 @@
 import { factory, representations } from '..';
 
-import { jsonld as jsonldFixtures } from '../__fixtures__';
+import {
+  json as jsonFixtures,
+  jsonld as jsonldFixtures,
+} from '../__fixtures__';
 
 it('can produce application/did+ld+json', async () => {
   const didDocument = factory.build({
@@ -23,4 +26,34 @@ it('can consume application/did+ld+json', async () => {
     Buffer.from(JSON.stringify(jsonldFixtures.example1, null, 2))
   );
   expect((didDocument.entries as any).id).toBe('did:example:123');
+});
+
+it('cannot produce application/did+ld+json from application/did+json', async () => {
+  expect.assertions(1);
+  const didDocument = factory.build({
+    entries: {
+      ...jsonFixtures.example1,
+    },
+  });
+  didDocument.addRepresentation(representations);
+  try {
+    didDocument.produce('application/did+ld+json');
+  } catch (e) {
+    expect(e.message).toBe('@context is required and not present.');
+  }
+});
+
+it('can produce application/did+ld+json from application/did+json after adding @context', async () => {
+  const didDocument = factory.build({
+    entries: {
+      ...jsonFixtures.example1,
+    },
+  });
+  didDocument.addRepresentation(representations);
+  didDocument.assign({
+    '@context': 'https://www.w3.org/ns/did/v1',
+  });
+  const serialization = didDocument.produce('application/did+ld+json');
+  // not the map order does not matter
+  expect(JSON.parse(serialization.toString())).toEqual(jsonldFixtures.example1);
 });
--- packages/data-model/src/representations/did-ld-json.ts
+++ packages/data-model/src/representations/did-ld-json.ts
@@ -7,6 +7,9 @@ import {
 export const produceJson: DidDocumentProducer = function(
   entries: AbstractDataModel<object>
 ): string {
+  if (!(entries as any)['@context']) {
+    throw new Error('@context is required and not present.');
+  }
   // TODO: security checks for sanitization
   return JSON.stringify(entries, null, 2);
 };
@@ -15,11 +18,15 @@ export const consumeJson: DidDocumentConsumer = function(
   representation: Buffer
 ): AbstractDataModel<object> {
   // TODO: security checks for sanitization
-  return JSON.parse(representation.toString());
+  const entries = JSON.parse(representation.toString());
+  if (!entries['@context']) {
+    throw new Error('@context is required and not present.');
+  }
+  return entries;
 };
 
 export const representation = {
-  contentType: 'application/did+json',
+  contentType: 'application/did+ld+json',
   produce: produceJson,
   consume: consumeJson,
 };
