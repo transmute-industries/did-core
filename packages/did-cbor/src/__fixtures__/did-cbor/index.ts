8343e6e5c21b71166942ec990a3efe0277e23df5
Orie Steele <orie@transmute.industries>
2021-02-13 13:34:18 -0600

    fix: cbor fixture

--- packages/did-cbor/src/__fixtures__/did-cbor/index.ts
+++ packages/did-cbor/src/__fixtures__/did-cbor/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-export const dagCbor = {
+export const cbor = {
   example1: fs.readFileSync(path.resolve(__dirname, './example-1.cbor')),
   example2: fs.readFileSync(path.resolve(__dirname, './example-2.cbor')),
 };
