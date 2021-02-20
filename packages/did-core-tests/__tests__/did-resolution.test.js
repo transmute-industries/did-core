1be1784e15b3629c0932c0193717613e4c83a4a6
Orie Steele <orie@transmute.industries>
2021-02-20 13:34:39 -0600

    feat: add resolution

---
+++ packages/did-core-tests/__tests__/did-resolution.test.js
@@ -0,0 +1,27 @@
+describe('DID Resolution', () => {
+  describe('7.1', () => {
+    describe(`If the resolution is successful, this MUST be a metadata structure. 
+      This structure contains metadata about the DID document contained in the didDocument property. 
+      This metadata typically does not change between invocations of the resolve and 
+      resolveRepresentation functions unless the DID document changes, as it 
+      represents metadata about the DID document. If the resolution is unsuccessful, 
+      this output MUST be an empty metadata structure. Properties defined by this 
+      specification are in §7.1.3 DID Document Metadata.`, () => {
+      test('didDocumentMetadata is none empty of successful resolution', () => {
+        const didDocumentMetadata = {
+          deactivated: true,
+        };
+        expect(Object.keys(didDocumentMetadata).length).toBeGreaterThan(0);
+      });
+
+      test('didDocumentMetadata is must be empty if resolution is unsuccessful', () => {
+        const didResolutionMetadata = {
+          error: 'invalidDid',
+        };
+        const didDocumentMetadata = {};
+        expect(Object.keys(didDocumentMetadata).length).toBe(0);
+        expect(Object.keys(didResolutionMetadata).length).toBe(1);
+      });
+    });
+  });
+});
