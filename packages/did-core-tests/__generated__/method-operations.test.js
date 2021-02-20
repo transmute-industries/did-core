b6e20df3b671ab3e8a54c2b9b7fb43fcb056154f
Orie Steele <orie@transmute.industries>
2021-02-20 13:52:59 -0600

    fix: missing case

--- packages/did-core-tests/__generated__/method-operations.test.js
+++ packages/did-core-tests/__generated__/method-operations.test.js
@@ -1,5 +1,21 @@
 describe('Method Operations', () => {
   describe('8.2', () => {
+    describe('A DID method specification MUST define how authorization is performed to execute all operations, including any necessary cryptographic processes.', () => {
+      test.todo('positive');
+      test.todo('negative');
+    });
+    describe('A DID method specification MUST specify how a DID controller creates a DID and its associated DID document.', () => {
+      test.todo('positive');
+      test.todo('negative');
+    });
+    describe('A DID method specification MUST specify how a DID resolver uses a DID to resolve a DID document, including how the DID resolver can verify the authenticity of the response.', () => {
+      test.todo('positive');
+      test.todo('negative');
+    });
+    describe('A DID method specification MUST specify what constitutes an update to a DID document and how a DID controller can update a DID document or state that updates are not possible.', () => {
+      test.todo('positive');
+      test.todo('negative');
+    });
     describe('The DID method specification MUST specify how a DID controller can deactivate a DID or state that deactivation is not possible.', () => {
       test.todo('positive');
       test.todo('negative');
--- packages/did-core-tests/__tests__/conformance.test.js
+++ packages/did-core-tests/__tests__/conformance.test.js
@@ -3,6 +3,34 @@ const { representation } = require('@did-core/did-ld-json');
 
 describe('Conformance', () => {
   describe('1.4', () => {
+    describe(`A conforming producer is any algorithm realized as software 
+      and/or hardware that generates conforming DIDs or conforming DID Documents. 
+      A conforming producer MUST NOT produce non-conforming DIDs or DID documents.`, () => {
+      test('can produce JSON-LD', async () => {
+        const didDocument = await factory
+          .build()
+          .addRepresentation({ 'application/did+ld+json': representation })
+          .assign({
+            '@context': 'https://www.w3.org/ns/did/v1',
+            id: 'did:example:123',
+          })
+          .produce('application/did+ld+json');
+        expect(didDocument).toBeDefined();
+      });
+      test('cannot produce non conformant JSON-LD', async () => {
+        try {
+          await factory
+            .build()
+            .addRepresentation({ 'application/did+ld+json': representation })
+            .assign({
+              id: 'did:example:123',
+            })
+            .produce('application/did+ld+json');
+        } catch (e) {
+          expect(e.message).toBe('"@context" is required and not present.');
+        }
+      });
+    });
     describe(`A conforming consumer is any algorithm realized as software 
       and/or hardware that consumes conforming DIDs or conforming DID documents. 
       A conforming consumer MUST produce errors when 
