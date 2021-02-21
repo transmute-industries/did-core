563c1dc032af6b2675e831bfda4a7db883904cc8
Orie Steele <orie@transmute.industries>
2021-02-20 21:04:37 -0600

    chore: add test report

--- packages/did-core-tests/jest.config.js
+++ packages/did-core-tests/jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   testPathIgnorePatterns: ['/__generated__'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '../../docs',
+        filename: 'index.html',
+        expand: true,
+      },
+    ],
+  ],
 };
