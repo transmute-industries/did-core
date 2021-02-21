3cbdacf79cdbbad7475f27ef1925a1e94b36a6ba
Orie Steele <orie@transmute.industries>
2021-02-20 18:26:39 -0600

    chore: add untestable

---
+++ packages/did-core-tests/__tests__/privacy-requirements.test.js
@@ -0,0 +1,11 @@
+describe('Privacy Requirements', () => {
+  describe('8.4', () => {
+    describe(`The DID method specification's Privacy Considerations section 
+    MUST discuss any subsection of Section 5 of [RFC6973] that could apply 
+    in a method-specific manner. The subsections to consider are: surveillance, 
+    stored data compromise, unsolicited traffic, misattribution, correlation, 
+    identification, secondary use, disclosure, and exclusion.`, () => {
+      test.todo('untestable');
+    });
+  });
+});
