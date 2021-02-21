f5da3db050ba148e2594cbacaa21b6d8652faf61
Orie Steele <orie@transmute.industries>
2021-02-20 19:05:41 -0600

    chore: add untestable

---
+++ packages/did-core-tests/__tests__/security-requirements.test.js
@@ -0,0 +1,67 @@
+describe('Security Requirements', () => {
+  describe('8.3', () => {
+    describe(`A DID method specifications MUST follow all guidelines and 
+      normative language provided in RFC3552: Writing Security Considerations Sections 
+      for the DID operations defined in the DID method specification.`, () => {
+      test.todo('untestable');
+    });
+    describe(`The Security Considerations section MUST document the 
+      following forms of attack for the DID operations defined in the 
+      DID method specification: eavesdropping, replay, message insertion, 
+      deletion, modification, denial of service, storage or network amplification, 
+      and man-in-the-middle. Other known forms of attack SHOULD also be documented.`, () => {
+      test.todo('untestable');
+    });
+    describe(`The Security Considerations section MUST discuss residual risks, 
+      such as the risks from compromise in a related protocol, incorrect implementation, 
+      or cipher after threat mitigation was deployed.`, () => {
+      test.todo('untestable');
+    });
+    describe(`The Security Considerations section MUST provide integrity 
+      protection and update authentication for all operations required by 
+      Section §8.2 Method Operations.`, () => {
+      test.todo('untestable');
+    });
+    describe(`If authentication is involved, particularly user-host authentication, 
+      the security characteristics of the authentication method MUST be clearly documented.`, () => {
+      test.todo('untestable');
+    });
+    describe(`The Security Considerations section MUST discuss the 
+      policy mechanism by which DIDs are proven to be uniquely assigned.`, () => {
+      test.todo('untestable');
+    });
+    describe(`Method-specific endpoint authentication MUST be discussed. 
+      Where DID methods make use of DLTs with varying network topology, 
+      sometimes offered as light node or  thin client implementations 
+      to reduce required computing resources, the security assumptions 
+      of the topology available to implementations of the DID method MUST be discussed.`, () => {
+      test.todo('untestable');
+    });
+    describe(`If a protocol incorporates cryptographic protection mechanisms, 
+      the DID method specification MUST clearly indicate which portions of 
+      the data are protected and by what protections, and it SHOULD give 
+      an indication of the sorts of attacks to which the cryptographic 
+      protection is susceptible. Some examples are integrity only, 
+      confidentiality, and endpoint authentication.`, () => {
+      test.todo('untestable');
+    });
+    describe(`Data which is to be held secret (keying material, random seeds, and so on) 
+      SHOULD be clearly labeled.`, () => {
+      test.todo('untestable');
+    });
+    describe(`DID method specifications SHOULD explain and specify the implementation of 
+      signatures on DID documents, if applicable.`, () => {
+      test.todo('untestable');
+    });
+    describe(`Where DID methods use peer-to-peer computing resources, 
+      such as with all known DLTs, the expected burdens of those resources 
+      SHOULD be discussed in relation to denial of service.`, () => {
+      test.todo('untestable');
+    });
+    describe(`DID methods that introduce new authentication service types, 
+      as described in §5.4 Services, SHOULD consider the security requirements 
+      of the supported authentication protocol.`, () => {
+      test.todo('untestable');
+    });
+  });
+});
