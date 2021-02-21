542eb8468d28aed3ad3af7f42052fbcfabb7f931
Orie Steele <orie@transmute.industries>
2021-02-20 18:09:37 -0600

    chore: key agreement

---
+++ packages/did-core-tests/__tests__/key-agreement.test.js
@@ -0,0 +1,22 @@
+const Ajv = require('ajv');
+const {
+  assertVerificationRelationship,
+} = require('../test-helpers/assertVerificationRelationship');
+
+const ajv = new Ajv({ strict: false });
+
+ajv.addSchema(require('../json-schemas/didUrl.json'));
+ajv.addSchema(require('../json-schemas/Jwk.json'));
+ajv.addSchema(require('../json-schemas/verificationMethod.json'));
+ajv.addSchema(require('../json-schemas/keyAgreement.json'));
+
+describe('Key Agreement', () => {
+  describe('5.3.3', () => {
+    describe(`The keyAgreement property is OPTIONAL. 
+      If present, the associated value MUST be an ordered set 
+      of one or more verification methods. 
+      Each verification method MAY be embedded or referenced.`, () => {
+      assertVerificationRelationship('keyAgreement', ajv);
+    });
+  });
+});
