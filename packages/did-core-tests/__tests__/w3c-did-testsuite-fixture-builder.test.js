42769a3be9cc4a88056f1a6e207153f424aaa0b4
Orie Steele <orie@transmute.industries>
2021-05-31 12:13:03 -0500

    fix: w3c testsuite fixtures

--- packages/did-core-tests/__tests__/w3c-did-testsuite-fixture-builder.test.js
+++ packages/did-core-tests/__tests__/w3c-did-testsuite-fixture-builder.test.js
@@ -111,7 +111,7 @@ describe('W3C DID Test Suite Transmute Implementations', () => {
           // we choose to suppport JWK and did+json as the default for our implementations.
           if (didDocument.verificationMethod[0].type === 'JsonWebKey2020') {
             didMethodFixture[didDocument.id].didDocumentDataModel = {
-              ...parsedDidDocumentStreamWithoutContext,
+              properties: parsedDidDocumentStreamWithoutContext,
             };
           }
 
@@ -137,12 +137,24 @@ describe('W3C DID Test Suite Transmute Implementations', () => {
         // did:web, did:elem, did:photon all produce did+json that is valid did+ld+json.
         // update the fixtures accordinging.
         if (!didMethodFixture[didDocument.id]['application/did+ld+json']) {
-          didMethodFixture[didDocument.id]['application/did+ld+json'] =
-            didMethodFixture[didDocument.id]['application/did+json'];
+          didMethodFixture[didDocument.id][
+            'application/did+ld+json'
+          ] = JSON.parse(
+            JSON.stringify(
+              didMethodFixture[didDocument.id]['application/did+json']
+            )
+          );
+
           didMethodFixture[didDocument.id][
             'application/did+ld+json'
           ].didResolutionMetadata = {
             contentType: 'application/did+ld+json',
+          };
+
+          didMethodFixture[didDocument.id][
+            'application/did+json'
+          ].didResolutionMetadata = {
+            contentType: 'application/did+json',
           };
         }
       } catch (e) {
